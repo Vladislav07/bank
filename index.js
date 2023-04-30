16b2b5e4ec3d1ea80dfa2e2df2e739a083f0589e
Владислав Белов <belov_vlad@list.ru>
2023-04-30 16:09:51 +0300

    01

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import { authorizationRequest } from "./server_access";
-console.log('start')
-authorizationRequest();
+import {
+  authorizationRequest,
+  listOfUserAccounts,
+  accountDetails,
+} from './server_access.js';
+
+console.log('start');
+
+authorizationRequest().then((token) => {
+  listOfUserAccounts(token).then((id) => {
+    accountDetails(token, id);
+  });
+});
--- server_access.js
+++ server_access.js
@@ -1,21 +1,80 @@
+import fetch from 'node-fetch';
+
+const user = {
+  login: 'developer',
+  password: 'skillbox',
+};
+
+
 export async function authorizationRequest() {
   try {
-    const res = await fetch("http://localhost:3000/login", {
+    console.log('authorizationRequest');
+    const res = await fetch('http://localhost:3000/login', {
       method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      body: {
-        login: 'developer',
-        password: 'skillbox'
-      }
+      body: JSON.stringify(user),
     });
     if (res.ok == true) {
       const data = await res.json();
-      console.log(data);
+      if (data.error) {
+        throw new Error(data.error);
+      } else {
+       const token = data.payload.token;     
+        console.log(token);
+        return token;
+      }
     }
-
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
+  }
+}
+
+export async function listOfUserAccounts(token) {
+  try {
+    console.log('listOfUserAccounts');
+    const res = await fetch('http://localhost:3000/accounts', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${token}`,
+      },
+    });
+    if (res.ok == true) {
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      } else {
+        console.log(data.payload);
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export async function accountDetails(token,Account) {
+
+  const idAccount = Account[1].account
+  try {
+    console.log('accountDetails');
+    const res = await fetch(`http://localhost:3000/account/${idAccount}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${token}`,
+      },
+    });
+    if (res.ok == true) {
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      } else {
+        console.log(data.payload);
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
   }
 }
