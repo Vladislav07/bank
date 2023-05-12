1b8a07f0e84ca56d684a100d0d342da24c5da09f
Владислав Белов <belov_vlad@list.ru>
2023-05-12 21:21:26 +0300

    12

--- src/account/account.js
+++ src/account/account.js
@@ -4,9 +4,9 @@ import "./_account.scss";
 import createTransaction from "./createTransaction";
 import { accountDetails } from "../utils/server_access";
 import drawChart from "../utils/charts";
-import {SortDataTransaction} from '../utils/charts'
+import { SortDataTransaction } from "../utils/charts";
 import { GoogleCharts } from "../utils/googleCharts.esm";
-import router from '../index';
+import router from "../index";
 
 import {
   Page,
@@ -21,178 +21,55 @@ import {
   Link,
 } from "../base/base";
 
-
-import currency from "currency.js";
-
 GoogleCharts.load(drawChart);
 let balanceUser = null;
-
 
 function detailsAccount(number) {
   const prefix = "account";
   const accountPage = new Page(prefix);
   const container = new Container(prefix);
+  const sectionChart = new Section(`${prefix}__chart`);
   const tag = el(`.${prefix}__chart`, {
     id: "chart",
   });
 
   accountDetails(number).then((data) => {
+    console.log(data);
     balanceUser = SortDataTransaction(
       data.transactions,
       data.balance,
       data.account
-    )
-      .reverse()
-      .slice(0, 5);
+    );
 
+    const count = balanceUser.length;
+    mount(sectionChart, tag);
     setChildren(container, [
       new Group(prefix, [
         new SectionTitle("Просмотр счета", prefix),
         new Link("/list", "Вернуться назад", prefix),
         el(`h3.${prefix}__number`, number),
-        //  el(`span.${prefix}__balance`, `${balance} руб.`),
+        el(`.${prefix}__balance`, [
+          el(`span.${prefix}__label`, `Баланс`),
+          el(`span.${prefix}__amount`, `${data.balance} руб.`),
+        ]),
       ]),
       createTransaction(prefix),
-      tag,
+      sectionChart,
       history(data.transactions, prefix),
     ]);
     setTimeout(() => {
-   
-      drawChart(balanceUser, tag);
-      
-
-    }, 1500);
+      drawChart(balanceUser.slice(count - 6, count), tag);
+    }, 300);
   });
-
   mount(accountPage, container);
+
   document.addEventListener("DOMContentLoaded", () => {
-    const chart = document.getElementById('chart');
-      chart.addEventListener('click', (e)=>{
-        e.preventDefault();
-        router.navigate(`balance/${number}`)
-      })
-  })
+    sectionChart.addEventListener("click", (e) => {
+      e.preventDefault();
+      router.navigate(`/balance/${number}`);
+    });
+  });
   return accountPage;
 }
 
-// function SortDataTransaction(transaction, amount, account) {
-//   let currentBalance = amount;
-//   let mongthLastTrans = getMongth(transaction[0].date);
-//   const dataChart = [];
-//   let outgo = 0;
-//   let receipt = 0;
-//   transaction.forEach((trans) => {
-//     if (getMongth(trans.date) === mongthLastTrans) {
-//       if (isOut(trans.from, account)) {
-//         outgo = currency(outgo).add(trans.amount).value;
-//       } else {
-//         receipt = currency(receipt).add(trans.amount).value;
-//       }
-//     } else {
-//       currentBalance = currency(currentBalance)
-//         .add(receipt)
-//         .subtract(outgo).value;
-
-//       const temp = {
-//         mongtn: getMonth(mongthLastTrans),
-//         out: outgo,
-//         in: receipt,
-//         balance: currentBalance,
-//       };
-
-//       dataChart.push(temp);
-//     // currentBalance = tempBalance;
-//       outgo = 0;
-//       receipt = 0;
-//       mongthLastTrans = getMongth(trans.date);
-//     }
-//   });
-//   return dataChart;
-// }
-
-// function getMongth(str) {
-//   const date = new Date(str);
-//   return date.getMonth();
-// }
-
-// function isOut(transaction, account) {
-//   if (account === transaction) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// GoogleCharts.load(drawChart);
-
-// function drawChart() {
-//   const monthlySummary = [];
-//   balanceUser.forEach((element) => {
-//     let temp = [element.mongtn, element.balance];
-//     monthlySummary.push(temp);
-//   });
-//   const data = dataChart();
-//   const chart = new GoogleCharts.api.visualization.ColumnChart(
-//     document.getElementById("chart")
-//   );
-//   chart.draw(data);
-// }
-
 export { detailsAccount as default };
-
-// function dataChart() {
-//   const monthlySummary = [];
-//   var data = new google.visualization.DataTable();
-//   data.addColumn("string", "Mongth");
-//   data.addColumn("number", "Balance");
-//   balanceUser.forEach((element) => {
-//     let temp = [element.mongtn, element.balance];
-//     monthlySummary.push(temp);
-//   });
-//   data.addRows(monthlySummary);
-//   return data;
-// }
-
-
-
-// function getMonth(number) {
-//  switch (number) {
-//   case 1:
-//    return "Январь"
-//     break;
-//     case 2:
-//    return"февраль"
-//     break;
-//     case 3:
-//    return "Март"
-//     break;
-//     case 4:
-//    return "Апрель"
-//     break;
-//     case 5:
-//    return "Май"
-//     break;
-//     case 6:
-//    return "июнь"
-//     break;
-//     case 7:
-//    return "июль"
-//      break;
-//     case 8:
-//    return "август"
-//     break;
-//     case 9:
-//    return "сентябрь"
-//     break;
-//     case 10:
-//    return "октябрь"
-//     break;
-//     case 11:
-//    return "ноябрь"
-//     break;
-//     case 12:
-//    return "декабрь"
-//     break;
-//   default:
-//     break;
-//  }
-// }
--- src/utils/charts.js
+++ src/utils/charts.js
@@ -3,11 +3,11 @@ import { GoogleCharts } from "./googleCharts.esm";
 
 import currency from "currency.js";
 
-GoogleCharts.load(drawChart);
+  GoogleCharts.load(drawChart);
 
-
-function drawChart(balanceUser, tag) {
+ function drawChart(balanceUser, tag) {
   const monthlySummary = [];
+
   if (balanceUser) {
     const count = balanceUser.length
     for (let index = 0; index < count; index++) {
@@ -18,7 +18,7 @@ function drawChart(balanceUser, tag) {
 
     const data = dataChart(monthlySummary);
     const chart = new GoogleCharts.api.visualization.ColumnChart(
-     tag //document.getElementById("chart")
+     tag
     );
     chart.draw(data);
   }
@@ -27,44 +27,49 @@ export { drawChart as default };
 
 
 export function SortDataTransaction(transaction, amount, account) {
+  const trans = transaction.map((record) => {
+    return {
+    from: record.from,
+     to: record.to,
+    date: new Date (record.date)
+    }
+  })
+  
+  console.log(trans)
   let currentBalance = amount;
-  let mongthLastTrans = getMongth(transaction[0].date);
+  let mongthLastTrans =  new Date(transaction[0].date).getMonth();
   const dataChart = [];
   let outgo = 0;
   let receipt = 0;
   transaction.forEach((trans) => {
-    if (getMongth(trans.date) === mongthLastTrans) {
+    if ( new Date(trans.date).getMonth() === mongthLastTrans) {
       if (isOut(trans.from, account)) {
         outgo = currency(outgo).add(trans.amount).value;
       } else {
         receipt = currency(receipt).add(trans.amount).value;
       }
     } else {
+
       currentBalance = currency(currentBalance)
         .add(receipt)
         .subtract(outgo).value;
 
       const temp = {
-        mongtn: getMonth(mongthLastTrans),
+        mongtn: months[mongthLastTrans],
         out: outgo,
         in: receipt,
         balance: currentBalance,
       };
 
       dataChart.push(temp);
-      // currentBalance = tempBalance;
       outgo = 0;
       receipt = 0;
-      mongthLastTrans = getMongth(trans.date);
+      mongthLastTrans = new Date(trans.date).getMonth();
     }
   });
   return dataChart;
 }
 
-function getMongth(str) {
-  const date = new Date(str);
-  return date.getMonth();
-}
 
 function isOut(transaction, account) {
   if (account === transaction) {
@@ -74,58 +79,15 @@ function isOut(transaction, account) {
 }
 
 
-
-
 function dataChart(monthlySummary) {
   var data = new google.visualization.DataTable();
   data.addColumn("string", "Mongth");
   data.addColumn("number", "Balance");
- 
+
   data.addRows(monthlySummary);
   return data;
 }
 
 
+  const months = [ "Январь","февраль", "Март", "Апрель", "Май", "июнь","июль", "август","сентябрь","октябрь","ноябрь", "декабрь"]
 
-function getMonth(number) {
-  switch (number) {
-    case 1:
-      return "Январь"
-      break;
-    case 2:
-      return "февраль"
-      break;
-    case 3:
-      return "Март"
-      break;
-    case 4:
-      return "Апрель"
-      break;
-    case 5:
-      return "Май"
-      break;
-    case 6:
-      return "июнь"
-      break;
-    case 7:
-      return "июль"
-      break;
-    case 8:
-      return "август"
-      break;
-    case 9:
-      return "сентябрь"
-      break;
-    case 10:
-      return "октябрь"
-      break;
-    case 11:
-      return "ноябрь"
-      break;
-    case 12:
-      return "декабрь"
-      break;
-    default:
-      break;
-  }
-}
