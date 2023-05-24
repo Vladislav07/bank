fa51ccac5766f44c84d01a5911e622521679db7d
Владислав Белов <belov_vlad@list.ru>
2023-05-24 19:53:06 +0300

    [

--- src/account/accountController.js
+++ src/account/accountController.js
@@ -14,14 +14,14 @@ const body = document.querySelector("#root");
 
 export default function AccountController(number) {
   const page = new AccountPage(body);
-  
+
   accountDetails(number).then((data) => {
     if(data.transactions.length ===0){
       page.renderBody(data.account, 0, [], [])
     }
     else{
      const formatDate = SortDataTransaction(data.transactions, data.amount, data.account);
-     page.renderBody(data.account, data.amount, TransformationTrans(data.transactions), formatDate) 
+     page.renderBody(data.account, data.amount, TransformationTrans(data.transactions), formatDate)
     }
     SetSelect()
   })
@@ -111,54 +111,67 @@ function TransformationTrans(trans) {
 
 function SetSelect() {
   const elem = document.getElementById("destinationAccount");
-  const select = new Choices(elem, {
-    silent: false,
-    items: [],
-    choices: [],
-    renderChoiceLimit: -1,
-    maxItemCount: -1,
-    addItems: true,
-    addItemFilter: null,
-    removeItems: true,
-    removeItemButton: false,
-    editItems: false,
-    allowHTML: false,
-    duplicateItemsAllowed: true,
-    delimiter: ",",
-    paste: false,
-    searchEnabled: false,
-    searchChoices: true,
-    searchFloor: 1,
-    searchResultLimit: 3,
-    searchFields: ["label", "value"],
-    position: "auto",
-    resetScrollPosition: true,
-    shouldSort: true,
-    shouldSortItems: false,
-    placeholder: false,
-    placeholderValue: null,
-    searchPlaceholderValue: null,
-    prependValue: null,
-    appendValue: null,
-    renderSelectedChoices: "auto",
-    itemSelectText: "",
-  });
 
-  select.setValue([
-    { value: "account", label: "По номеру" },
-    { value: "balance", label: "По балансу" },
-    { value: "transaction.date", label: "По последней транзакции" },
+
+  const choices = new Choices(elem,
     {
-      value: "placeholder",
-      label: "Сортировка",
-      selected: false,
-      disabled: false,
-    },
-  ]);
+      noChoicesText:'jjj'
+    });
 
- 
+  // const select = new Choices(elem, {
+  //   silent: false,
+  //   items: [],
+  //   choices: [],
+  //   renderChoiceLimit: -1,
+  //   maxItemCount: 1,
+  //   addItems: true,
+  //   addItemFilter: null,
+  //   removeItems: true,
+  //   removeItemButton: false,
+  //   editItems: false,
+  //   allowHTML: true,
+  //   duplicateItemsAllowed: true,
+  //   delimiter: ",",
+  //   paste: true,
+  //   searchEnabled: false,
+  //   searchChoices: true,
+  //   searchFloor: 1,
+  //   searchResultLimit: 3,
+  //   searchFields: ["label", "value"],
+  //   position: "auto",
+  //   resetScrollPosition: true,
+  //   shouldSort: true,
+  //   shouldSortItems: false,
+  //   placeholder: false,
+  //   placeholderValue: null,
+  //   searchPlaceholderValue: null,
+  //   prependValue: null,
+  //   appendValue: null,
+  //   renderSelectedChoices: "auto",
+  //   itemSelectText: "",
+  //   addItemText: (value) => {
+  //     return value;
+  //   },
 
-  select.passedElement.element.addEventListener("change", (value) => {
+  // });
+
+  //choices.setChoiceByValue('Two');
+
+  // select.setValue([
+  //   { value: "account", label: "По номеру" },
+  //   { value: "balance", label: "По балансу" },
+  //   { value: "transaction.date", label: "По последней транзакции" },
+  //   {
+  //     value: "placeholder",
+  //     label: "Сортировка",
+  //     selected: false,
+  //     disabled: false,
+  //   },
+  // ]);
+
+
+
+  choices.passedElement.element.addEventListener("change", (value) => {
 
     console.log('arrAccount');
   });
